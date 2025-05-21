91a2ea3e21e15efccc1707d39627ad68c9b7c099
bytesunite <bytesunite@gmail.com>
2025-05-20 22:58:51 -0700

    create new pages and routes
    pkg react-router-dom can create routes for individual pages for a React app using a browser router

--- fullstack-react-app/front-end/src/App.jsx
+++ fullstack-react-app/front-end/src/App.jsx
@@ -1,8 +1,33 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage.jsx';
+import AboutPage from './pages/AboutPage.jsx';
+import ArticlePage from './pages/ArticlePage.jsx';
+import ArticlesListPage from './pages/ArticlesListPage.jsx';
+
+const routes = [
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/about/',
+    element: <AboutPage />,
+  },
+  {
+    path: '/articles',
+    element: <ArticlesListPage />,
+  },
+  {
+    path: '/articles/individual',
+    element: <ArticlePage />,
+  },
+];
+
+const router = createBrowserRouter(routes);
 
 function App() {
-  return <HomePage />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ fullstack-react-app/front-end/src/pages/ArticlesListPage.jsx
@@ -0,0 +1,3 @@
+export default function ArticlesListPage() {
+  return <h1>This is the Articles List Page!</h1>;
+}
