0eaf9df63013d2431e6a3848a2ab17e919d1d711
bytesunite <bytesunite@gmail.com>
2025-05-22 01:06:55 -0700

    create links to other pages
    the Link component allows linking to other routes without page reloading that happends with HTML a tags

--- fullstack-react-app/front-end/src/App.jsx
+++ fullstack-react-app/front-end/src/App.jsx
@@ -4,23 +4,30 @@ import HomePage from './pages/HomePage.jsx';
 import AboutPage from './pages/AboutPage.jsx';
 import ArticlePage from './pages/ArticlePage.jsx';
 import ArticlesListPage from './pages/ArticlesListPage.jsx';
+import Layout from './Layout.jsx';
 
 const routes = [
   {
     path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/about/',
-    element: <AboutPage />,
-  },
-  {
-    path: '/articles',
-    element: <ArticlesListPage />,
-  },
-  {
-    path: '/articles/individual',
-    element: <ArticlePage />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/about/',
+        element: <AboutPage />,
+      },
+      {
+        path: '/articles',
+        element: <ArticlesListPage />,
+      },
+      {
+        path: '/articles/individual',
+        element: <ArticlePage />,
+      },
+    ],
   },
 ];
 
