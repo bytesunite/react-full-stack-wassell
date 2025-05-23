16fde5ddfcd3e170b5f99777f74feb0b246b6106
bytesunite <bytesunite@gmail.com>
2025-05-22 22:02:04 -0700

    link url params to article
    match an article in a list of articles using a url paramter value

--- fullstack-react-app/front-end/src/pages/ArticlePage.jsx
+++ fullstack-react-app/front-end/src/pages/ArticlePage.jsx
@@ -1,7 +1,16 @@
 import { useParams } from 'react-router-dom';
+import articles from '../article-content.js';
 
 export default function ArticlePage() {
   const { name } = useParams();
+  const article = articles.find((a) => a.name === name);
 
-  return <h1>This is the {name} article</h1>;
+  return (
+    <>
+      <h3>{article.title}</h3>
+      {article.content.map((p) => (
+        <p key={p}>{p}</p>
+      ))}
+    </>
+  );
 }
