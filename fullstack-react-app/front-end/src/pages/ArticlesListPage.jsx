0fb815b52ac937c1f425f055eedc2fb08c94f353
bytesunite <bytesunite@gmail.com>
2025-05-22 22:38:53 -0700

    create a list of articles as links
    Display a list of articles as links to an individual article

--- fullstack-react-app/front-end/src/pages/ArticlesListPage.jsx
+++ fullstack-react-app/front-end/src/pages/ArticlesListPage.jsx
@@ -1,3 +1,16 @@
+import { Link } from 'react-router-dom';
+import articles from '../article-content.js';
+
 export default function ArticlesListPage() {
-  return <h1>This is the Articles List Page!</h1>;
+  return (
+    <>
+      <h1>Articles</h1>
+      {articles.map((a) => (
+        <Link key={a.name} to={'/articles/' + a.name}>
+          <h3>{a.title}</h3>
+          <p>{a.content[0].substring(0, 150)}</p>
+        </Link>
+      ))}
+    </>
+  );
 }
