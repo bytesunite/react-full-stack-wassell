729e959a7b5e5408dfad37afaf4e113ba4bf54d8
bytesunite <bytesunite@gmail.com>
2025-05-26 00:34:13 -0700

    Express endpoints and Postman
    Creating and Testing Express endpoints with Postman

--- fullstack-react-app/back-end/src/server.js
+++ fullstack-react-app/back-end/src/server.js
@@ -6,8 +6,14 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/hello', (req, res) => {
-  res.send('Hello!');
+  res.send('Hello from a GET endpoint!');
+});
+
+app.post('/hello', (req, res) => {
+  res.send('Hello ' + req.body.name + ' from a Post endpoint!');
 });
 
 app.listen(8000, () => {
