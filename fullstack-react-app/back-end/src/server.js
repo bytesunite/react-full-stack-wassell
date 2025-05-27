209542fa7da52692d63dd9d34c409acab4177f83
bytesunite <bytesunite@gmail.com>
2025-05-26 23:02:49 -0700

    increment article upvotes with url params

--- fullstack-react-app/back-end/src/server.js
+++ fullstack-react-app/back-end/src/server.js
@@ -4,10 +4,30 @@ import express from 'express';
 // Node.js CommonJS modules
 // const express = require('express');
 
+const articleInfo = [
+  { name: 'learn-react', upvotes: 0 },
+  { name: 'mongodb', upvotes: 0 },
+  { name: 'learn-node', upvotes: 0 },
+];
+
 const app = express();
 
 app.use(express.json());
 
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const article = articleInfo.find((a) => a.name === req.params.name);
+  article.upvotes += 1;
+
+  res.send(
+    'Success! The article ' +
+      req.params.name +
+      ' now has ' +
+      article.upvotes +
+      ' upvotes!'
+  );
+});
+
+/*
 app.get('/hello', (req, res) => {
   res.send('Hello from a GET endpoint!');
 });
@@ -21,6 +41,7 @@ app.get('/hello/:name', (req, res) => {
 app.post('/hello', (req, res) => {
   res.send('Hello ' + req.body.name + ' from a Post endpoint!');
 });
+*/
 
 app.listen(8000, () => {
   console.log('Server is listening on port 8000');
