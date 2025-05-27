5539aa328b3e2c285bafb0412a87f75ce1e7ff38
bytesunite <bytesunite@gmail.com>
2025-05-27 02:21:42 -0700

    add comments to articles
    Created endpoint to add comments to individual articles. npm pkg nodemon can auto restart the server to help with developer productivity

--- fullstack-react-app/back-end/src/server.js
+++ fullstack-react-app/back-end/src/server.js
@@ -5,9 +5,9 @@ import express from 'express';
 // const express = require('express');
 
 const articleInfo = [
-  { name: 'learn-react', upvotes: 0 },
-  { name: 'mongodb', upvotes: 0 },
-  { name: 'learn-node', upvotes: 0 },
+  { name: 'learn-react', upvotes: 0, comments: [] },
+  { name: 'mongodb', upvotes: 0, comments: [] },
+  { name: 'learn-node', upvotes: 0, comments: [] },
 ];
 
 const app = express();
@@ -18,13 +18,17 @@ app.post('/api/articles/:name/upvote', (req, res) => {
   const article = articleInfo.find((a) => a.name === req.params.name);
   article.upvotes += 1;
 
-  res.send(
-    'Success! The article ' +
-      req.params.name +
-      ' now has ' +
-      article.upvotes +
-      ' upvotes!'
-  );
+  res.send(article);
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articleInfo.find((a) => a.name === name);
+  article.comments.push({ postedBy, text });
+
+  res.json(article);
 });
 
 /*
